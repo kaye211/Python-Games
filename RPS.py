b1493c545927a4b3c2b714460866a90f691d2da7
Kaye <kay22ng@yahoo.com>
2018-06-26 16:01:12 -0500

    Add helper functions to RPS.py

--- RPS.py
+++ RPS.py
@@ -46,6 +46,13 @@ def number_to_name(number):
 
 
 def player_choice(): #does this need to take in a parameter?
+    """
+    This takes in the human players input and returns an integer value
+    0 - rock
+    1 - paper
+    2 - scissors
+
+    """
     done = True
     while done:
         choice = input("Please choose rock, paper or scissors: ")
@@ -54,30 +61,71 @@ def player_choice(): #does this need to take in a parameter?
         else:
          return name_to_number(choice)
          #break  #why is break reachable with a print but unreachablle with return?
-      
+        
 def computer_choice():
     """
     returns a randomly choosen integer - 0, 1, 2 for computer choice"
     """
-    return randint(0, 2)
+    check= randint(0, 2)
+    print(number_to_name(check))
+    return check 
+player1choice = player_choice()
+player2choice= computer_choice()
+# player1 = 4  #will be whoevergoes first
+# player2 = 5  #will go second
 
-
-def play_game(player1, player2):
-    if player1 == 0 and player2 == 2:
-       print("winner is", player1)
+def play_game(player1 , player2):
+    if player1choice == 0 and player2choice == 2:
+       print("rock smashes scissors, " + ' ' + player1+ 'wins')
        
-    elif player1 == 2 and player2 == 1:
-        print("Scissors cuts paper, the winner is: ", player1)
-    elif player1 ==1 and player2 == 0:
-        print("Paper covers rock", player1)
+    elif player1choice == 2 and player2choice == 1:
+        print("Scissors cuts paper, the winner, " + ' ' + player1+ 'wins')
+    elif player1choice ==1 and player2choice == 0:
+        print("Paper covers rock, "+  ' ' + player1+ 'wins')
+    elif player2choice == 0 and player1choice == 2:
+       print("Rock smashes scissors, " + ' ' +  player2+ 'wins')
+       
+    elif player2choice == 2 and player1choice == 1:
+        print("Scissors cuts paper,  "+ ' ' + player2+ 'wins')
+    elif player2choice == 1 and player1choice == 0:
+        print("Paper covers rock, " + ' ' + player2 + 'wins') 
     else:
-        print("Tie")
+        print("There is a Tie")
+
+play_game(player1= 'human', player2 = 'computer')
     
-    
+def is_First():
+    return(randint(0, 1))
+#Need to figure out how to select a random choice
+# turn = is_First()
+# humanP = player_choice()
+# if turn == 0: #human plays first, player1 = human
+#     player_choice()
+# else:
+#     computer_choice()
+
 
     
-# human_player = player_choice()
-# computer_player = computer_choice()
+# human_player = 3
+# computer_player = 4
+# start_game = True
+# while start_game():
+#     if randint(3, 4) == 3:
+#         #human plays first
+#     else
+#         #computer p;ats 
+# play_game(human_player, computer_player):
+#     result =
+#     print(result) 
+
+#     if  result == 3: 
+#         print(result) 
+#         player_choice()
+#     else:
+#         print(result, 2) 
+#         computer_choice()
+#         break
+
 #when I call play_game(), I will call it with human_player and compuer_Plater
 
 #randonmly select who goes first
@@ -96,7 +144,7 @@ def play_game(player1, player2):
 # print(number_to_name(2)) # output = scissors
 # print(number_to_name(3))
 #player_choice()
-print(player_choice())
+#print(player_choice())
 #print(computer_choice())
 
 
